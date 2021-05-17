3f1bbaa56574203f45553e72798498f9f916c64c
Stagnet <brucewayne128gb@protonmail.com>
2021-05-17 22:26:20 +0530

    create restaurant info component

---
+++ src/features/restaurants/component/restaurant_Info.component.js
@@ -0,0 +1,7 @@
+import React from "react";
+import {} from "react-native-paper";
+import { StyleSheet, Text, View, StatusBar, SafeAreaView } from "react-native";
+
+export const RestaurantInfo = () => {
+  return <Text>Restaurant Info</Text>;
+};
