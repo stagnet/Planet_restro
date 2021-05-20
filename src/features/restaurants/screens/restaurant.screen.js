da5deda9a337afe5a9c792001978012e98fa1454
Stagnet <brucewayne128gb@protonmail.com>
2021-05-20 21:08:08 +0530

    created restaurant view list screen

--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -1,31 +1,47 @@
-import React from "react";
-import { Searchbar } from "react-native-paper";
-import { StyleSheet, Text, View, StatusBar, SafeAreaView } from "react-native";
-import { RestaurantInfo } from "../component/restaurant_Info.component";
+import React from 'react';
+import { Searchbar } from 'react-native-paper';
+import { Text, View, StatusBar, SafeAreaView } from 'react-native';
+import { RestaurantInfoCard } from '../component/restaurant_InfoCard.component';
+import styled from 'styled-components/native';
+
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
+
+const SearchContainer = styled(View)`
+  padding: ${(props) => props.theme.space[3]};
+`;
+const RestaurantList = styled(View)`
+  flex: 1;
+  padding: ${(props) => props.theme.space[3]};
+`;
 
 export const RestaurantScreen = () => {
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeArea>
       {/* searchBar */}
-      <View style={styles.search}>
+      <SearchContainer>
         <Searchbar />
-      </View>
-      <View style={styles.list}>
-        <RestaurantInfo />
-      </View>
-    </SafeAreaView>
+      </SearchContainer>
+
+      {/* restaurant list */}
+      <RestaurantList>
+        <RestaurantInfoCard />
+      </RestaurantList>
+    </SafeArea>
   );
 };
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     marginTop: StatusBar.currentHeight,
+//   },
+//   search: {
+//     padding: 16,
+//   },
+//   list: {
+//     flex: 1,
+//     padding: 16,
+//   },
+// });
