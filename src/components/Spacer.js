839b042906a050e8f6c550049a2389aa7d27861c
Stagnet <brucewayne128gb@protonmail.com>
2021-05-25 17:26:13 +0530

    update spacer component coz previous one is having a bug for android.

--- src/components/Spacer.js
+++ src/components/Spacer.js
@@ -1,43 +1,38 @@
+import styled, { useTheme } from 'styled-components/native';
 import React from 'react';
-import { View } from 'react-native';
-import styled from 'styled-components/native';
 
-const TopSmall = styled(View)`
-  margintop: ${(props) => props.theme.space[1]};
-`;
-const TopMedium = styled(View)`
-  margintop: ${(props) => props.theme.space[2]};
-`;
-const TopLarge = styled(View)`
-  margintop: ${(props) => props.theme.space[3]};
-`;
+const sizeVariant = {
+  small: 1,
+  medium: 2,
+  large: 3,
+};
 
-const LeftSmall = styled(View)`
-  margintop: ${(props) => props.theme.space[1]};
+const positionVariant = {
+  top: 'marginTop',
+  left: 'marginLeft',
+  right: 'marginRight',
+  bottom: 'marginBottom',
+};
+
+const getVariant = (position, size, theme) => {
+  const sizeIndex = sizeVariant[size];
+  const property = positionVariant[position];
+  const value = theme.space[sizeIndex];
+
+  return `${property}:${value}`;
+};
+
+const SpacerView = styled.View`
+  ${(props) => props.variant}
 `;
-const LeftMedium = styled(View)`
-  margintop: ${(props) => props.theme.space[2]};
-`;
-const LeftLarge = styled(View)`
-  margintop: ${(props) => props.theme.space[3]};
-`;
-export const Spacer = ({ variant }) => {
-  if (variant === 'top.large') {
-    return <TopLarge />;
-  }
-  if (variant === 'top.medium') {
-    return <TopMedium />;
-  }
-  if (variant === 'top.small') {
-    return <TopSmall />;
-  }
-  if (variant === 'left.large') {
-    return <LeftLarge />;
-  }
-  if (variant === 'left.medium') {
-    return <LeftMedium />;
-  }
-  if (variant === 'left.small') {
-    return <LeftSmall />;
-  }
+export const Spacer = ({ position, size, children }) => {
+  const theme = useTheme();
+  const variant = getVariant(position, size, theme);
+
+  return <SpacerView variant={variant}>{children}</SpacerView>;
+};
+
+Spacer.defaultProps = {
+  position: 'top',
+  size: 'small',
 };
