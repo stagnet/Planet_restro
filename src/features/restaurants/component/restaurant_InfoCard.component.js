2f23f097424149f9da4d45f91e24c433f43a6ab5
Stagnet <brucewayne128gb@protonmail.com>
2021-06-28 15:49:45 +0530

    added favourite icon to the info card

--- src/features/restaurants/component/restaurant_InfoCard.component.js
+++ src/features/restaurants/component/restaurant_InfoCard.component.js
@@ -10,31 +10,41 @@ import {
   Info,
   RestaurantCover,
   Row,
+  Icon,
   Section,
 } from '../component/restaurant_infoCard.styles';
+import { Favourite } from '../../../components/favourites/favourite.component';
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = 'Sushi Town',
-    icon,
+    icon = 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
     photos = ['https://picsum.photos/400/300'],
     address = 'Fukushima umamoto town',
     isOpenNow = true,
     rating = 4,
     isClosedTemporarily = true,
+    placeId,
   } = restaurant;
 
+  // * rendering nuber of star on the basis of rating....
   const ratingArray = Array.from(new Array(Math.floor(rating)));
 
   return (
     <RestaurantCard elevation={5}>
+      <Favourite restaurant={restaurant} />
       <RestaurantCover key={name} source={{ uri: photos[0] }} />
       <Info>
         <Text variant='lable'>{name}</Text>
         <Section>
           <Row>
-            {ratingArray.map(() => (
-              <SvgXml xml={star} height={20} width={20} />
+            {ratingArray.map((_, index) => (
+              <SvgXml
+                key={`star-${placeId}-${index}`}
+                xml={star}
+                height={20}
+                width={20}
+              />
             ))}
           </Row>
           {isClosedTemporarily && (
@@ -44,6 +54,9 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
           <Row>
             <Spacer position='left' size='large'>
               {isOpenNow && <SvgXml xml={open} height={20} width={30} />}
+            </Spacer>
+            <Spacer position='left' size='large'>
+              <Icon source={{ uri: icon }} />
             </Spacer>
           </Row>
         </Section>
