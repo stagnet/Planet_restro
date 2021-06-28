38f02b5e15b3708f56a6fda40f3d69aebd228658
Stagnet <brucewayne128gb@protonmail.com>
2021-06-28 15:52:54 +0530

    refactoring and optimization in restaurant screen

--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import { Searchbar } from 'react-native-paper';
-import { View, FlatList } from 'react-native';
+import React, { useContext } from 'react';
+//todo; STEP4 - use the react context inside any component you want using 'useContext'
+import {
+  View,
+  FlatList,
+  ActivityIndicator,
+  TouchableOpacity,
+} from 'react-native';
 import { RestaurantInfoCard } from '../component/restaurant_InfoCard.component';
 import { Spacer } from '../../../components/spacer/Spacer';
 import styled from 'styled-components/native';
+
+import { Search } from '../component/searchbar.component';
 import { SafeArea } from '../../../utils/SafeArea/safeArea.component';
 
-const SearchContainer = styled(View)`
-  padding: ${(props) => props.theme.space[3]};
-`;
+import { RestaruantsContext } from '../../../services/restaurant/restarunt.context';
+// import { FavouritesContext } from '../../../services/favourites/favourites.context';
 
 //todo: using below technique we can apply styles to the internal element of a parent component....
 const RestaurantList = styled(FlatList).attrs({
@@ -17,35 +23,43 @@ const RestaurantList = styled(FlatList).attrs({
   },
 })``;
 
-export const RestaurantScreen = () => {
+export const RestaurantScreen = ({ navigation }) => {
+  const { restarauntsData, isLoading } = useContext(RestaruantsContext);
+  // const { favourites } = useContext(FavouritesContext);
   return (
     <SafeArea>
+      {isLoading && (
+        <View style={{ position: 'absolute', left: '50%', top: '50%' }}>
+          <ActivityIndicator
+            size='large'
+            color='#ff7417'
+            animating={true}
+            style={{ marginLeft: -20 }}
+          />
+        </View>
+      )}
+
       {/* searchBar */}
-      <SearchContainer>
-        <Searchbar />
-      </SearchContainer>
+      <Search />
 
       {/* restaurant list */}
 
       <RestaurantList
-        data={[
-          { id: '1' },
-          { id: '2' },
-          { id: '3' },
-          { id: '4' },
-          { id: '5' },
-          { id: '6' },
-          { id: '7' },
-        ]}
-        renderItem={() => (
+        data={restarauntsData}
+        renderItem={({ item }) => (
           <>
-            <Spacer position='bottom' size='large'>
-              <RestaurantInfoCard />
-            </Spacer>
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate('ReastaurantDetails', { restaurant: item })
+              }
+            >
+              <Spacer position='bottom' size='large'>
+                <RestaurantInfoCard restaurant={item} />
+              </Spacer>
+            </TouchableOpacity>
           </>
         )}
-        keyExtractor={(item) => item.id}
-        key={(item) => item.id}
+        keyExtractor={(item) => item.name}
       />
     </SafeArea>
   );
