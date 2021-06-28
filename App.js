c9c4c1e17701f3c32aaff341db1356aee8d60ea0
Stagnet <brucewayne128gb@protonmail.com>
2021-06-28 15:53:23 +0530

    packages upgraded and minor changes

--- App.js
+++ App.js
@@ -1,31 +1,19 @@
 import React from 'react';
-import { RestaurantScreen } from './src/features/restaurants/screens/restaurant.screen';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components/native';
 import { theme } from './src/infrasturcture/theme/index';
-import { Text } from 'react-native-paper';
+
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from '@expo-google-fonts/oswald';
 import { Lato_400Regular, useFonts as useLato } from '@expo-google-fonts/lato';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { SafeArea } from './src/utils/SafeArea/safeArea.component';
 
-const BottomTab = createBottomTabNavigator();
+import { RestaruantsContextProvider } from './src/services/restaurant/restarunt.context';
+import { LocationContextProvider } from './src/services/location/location.context';
+import { FavouritesContextProvider } from './src/services/favourites/favourites.context';
 
-const Settings = () => (
-  <SafeArea>
-    <Text>settings page</Text>
-  </SafeArea>
-);
-
-const Maps = () => (
-  <SafeArea>
-    <Text>map page</Text>
-  </SafeArea>
-);
+import { Navigation } from './src/infrasturcture/routes/index';
 
 export default function App() {
   const [oswald] = useOswald({ Oswald_400Regular });
@@ -37,13 +25,15 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <NavigationContainer>
-          <BottomTab.Navigator>
-            <BottomTab.Screen name='Restaurants' component={RestaurantScreen} />
-            <BottomTab.Screen name='Settings' component={Settings} />
-            <BottomTab.Screen name='Maps' component={Maps} />
-          </BottomTab.Navigator>
-        </NavigationContainer>
+        {/* //todo; STEP2 - import it to App level globally... */}
+        <FavouritesContextProvider>
+          <LocationContextProvider>
+            <RestaruantsContextProvider>
+              <Navigation />
+            </RestaruantsContextProvider>
+          </LocationContextProvider>
+        </FavouritesContextProvider>
+
         <ExpoStatusBar style='auto' />
       </ThemeProvider>
     </>
