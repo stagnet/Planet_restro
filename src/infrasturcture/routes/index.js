222927a29ce8790a389cc6859eeb527138944bdb
stagnet <brucewayne128gb@protonmail.com>
2021-11-05 20:15:02 +0530

    put navigator container to the index file

--- src/infrasturcture/routes/index.js
+++ src/infrasturcture/routes/index.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { View, Text } from 'react-native';
+
 import { AppNavigator } from './app.navigator';
+import { AccountNavigator } from './account.navigator';
+
+import { AuthenticationContext } from '../../services/authentication/auth.context.js';
 
 export const Navigation = () => {
-  return <AppNavigator />;
+  const { isAuthenticated } = useContext(AuthenticationContext);
+  return (
+    <NavigationContainer>
+      {isAuthenticated ? (
+        <AppNavigator />
+      ) : (
+        <AccountNavigator />
+        // <SafeArea>
+        //   <View>
+        //     <Text>Not Authenticated</Text>
+        //   </View>
+        // </SafeArea>
+      )}
+    </NavigationContainer>
+  );
 };
