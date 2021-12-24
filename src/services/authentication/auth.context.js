a1b5be267bc020e49c07a54243ec573e5888f0b8
stagnet <brucewayne128gb@protonmail.com>
2021-12-24 11:51:21 +0530

    created auth context

---
+++ src/services/authentication/auth.context.js
@@ -0,0 +1,74 @@
+import React, { useState, createContext } from 'react';
+import * as firebase from 'firebase';
+import { loginRequest } from './auth.service.js';
+
+export const AuthenticationContext = createContext();
+
+export const AuthenticationContextProvider = (props) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+
+  firebase.default.auth().onAuthStateChanged((usr) => {
+    if (usr) {
+      setUser(usr);
+      console.log('already logged in');
+      setIsLoading(false);
+    } else {
+      setIsLoading(false);
+    }
+  });
+
+  const onLogin = (email, passowrd) => {
+    setIsLoading(true);
+    loginRequest(email, passowrd)
+      .then((info) => {
+        setUser(info);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(err.toString());
+      });
+  };
+
+  const onRegister = (email, password, repeatedPassword) => {
+    setIsLoading(true);
+    if (password !== repeatedPassword) {
+      setError('Error: Password do not match');
+      return;
+    }
+    firebase.default
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((info) => {
+        setUser(info);
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        setIsLoading(false);
+        setError(e.toString());
+      });
+  };
+
+  const onLogout = () => {
+    setUser(null);
+    firebase.default.auth().signOut();
+  };
+
+  return (
+    <AuthenticationContext.Provider
+      value={{
+        isAuthenticated: !!user,
+        user,
+        isLoading,
+        error,
+        onLogin,
+        onRegister,
+        onLogout,
+      }}
+    >
+      {props.children}
+    </AuthenticationContext.Provider>
+  );
+};
