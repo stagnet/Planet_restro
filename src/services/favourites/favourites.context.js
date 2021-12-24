b4235289e822ba81b1714af0fd076e29be301431
stagnet <brucewayne128gb@protonmail.com>
2021-12-24 11:52:33 +0530

    implement authentication based favourites

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,23 +1,25 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useState, useEffect, useContext } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AuthenticationContext } from '../authentication/auth.context.js';
 
 export const FavouritesContext = createContext();
 
 export const FavouritesContextProvider = ({ children }) => {
   const [favourites, setFavourite] = useState([]);
+  const { user } = useContext(AuthenticationContext);
 
-  const saveFavourites = async (value) => {
+  const saveFavourites = async (value, uid) => {
     try {
       const json = JSON.stringify(value);
-      await AsyncStorage.setItem('@favourites', json);
+      await AsyncStorage.setItem(`@favourites+${uid}`, json);
     } catch (error) {
       console.error('error storing', error);
     }
   };
 
-  const loadFavourites = async () => {
+  const loadFavourites = async (uid) => {
     try {
-      const value = await AsyncStorage.getItem('@favourites');
+      const value = await AsyncStorage.getItem(`@favourites+${uid}`);
       if (value !== null) {
         setFavourite(JSON.parse(value));
       }
@@ -38,12 +40,16 @@ export const FavouritesContextProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    loadFavourites(favourites);
-  }, []);
+    if (user) {
+      loadFavourites(user.uid);
+    }
+  }, [user]);
 
   useEffect(() => {
-    saveFavourites(favourites);
-  }, [favourites]);
+    if (user) {
+      saveFavourites(favourites, user.uid);
+    }
+  }, [favourites, user]);
 
   return (
     <FavouritesContext.Provider
